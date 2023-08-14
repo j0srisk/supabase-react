e77d2a155208e3453301bb962195c2c5199e5d6c
j0srisk <j0srisk@gmail.com>
2023-08-14 16:15:26 -0500

    inital setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div>
-      <h1 className="font-bold">Template React Tailwind</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="font-bold">Supabase React</h1>
+		</div>
+	);
 }
 
 export default App;
